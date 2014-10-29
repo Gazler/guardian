78a50c30fe9877994454e676dddcd584a375e259
Sonny Scroggin <scrogson@gmail.com>
2014-10-28 23:11:54 -0500

    Dependencies

--- mix.exs
+++ mix.exs
@@ -8,23 +8,14 @@ defmodule Guardian.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :cowboy, :plug]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [
+      {:cowboy, "~> 1.0.0"},
+      {:plug, "~> 0.8.1"}
+    ]
   end
 end
